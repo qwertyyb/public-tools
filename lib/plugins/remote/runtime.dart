301a6237f030a99cfe07db7830b77d8a87021968
qwertyyb <qwertyyb@qq.com>
2022-05-08 12:27:09 +0800

    fix: 移除超时逻辑

--- lib/plugins/remote/runtime.dart
+++ lib/plugins/remote/runtime.dart
@@ -1,16 +1,7 @@
-import 'package:path_provider/path_provider.dart';
-
 import 'nodejs.dart';
 
-Future<String> _getPnpmStorePath() async {
-  final supportDir = await getApplicationSupportDirectory();
-  return '${supportDir.path}/pnpm-store';
-}
-
 Future<int> _installPnpm() async {
-  final process = await runWithBin('npm', ['install', '-g', 'pnpm']);
-  final storePath = await _getPnpmStorePath();
-  await runWithBin('pnpm', ['pnpm', 'config', 'set', 'store-dir', storePath]);
+  final process = await runWithBin('npm', ['install', '-g', 'pnpm@6']);
   return process.exitCode;
 }
 
